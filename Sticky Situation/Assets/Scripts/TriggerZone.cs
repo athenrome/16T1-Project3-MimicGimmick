6e7686b8168ae77cf6ba1e1164ad8a6b0dc03f2b
Jake Garrett <jake-garrett@hotmail.com>
2016-04-27 00:24:18 +1000

    Added Trigger Script
    Trigger Script added. Doesn't seem to be working yet but it should be?

---
+++ Sticky Situation/Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerZone : MonoBehaviour {
+
+	public int number = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter (Collider col)
+	{
+		if (col.tag == "Player") 
+		{
+			Debug.Log (number);
+			//IDK do some mimicing stuff probably
+
+		}
+
+	}
+}
