7eb64a3e2798c98d1ec12c2dfe4759180044ae4d
Jake Garrett <jake-garrett@hotmail.com>
2016-05-03 15:28:48 +1000

    fixed my stuff up.

--- Sticky Situation/Assets/Scripts/Player Scripts/Player.cs
+++ Sticky Situation/Assets/Scripts/Player Scripts/Player.cs
@@ -224,15 +224,4 @@ public class Player : MonoBehaviour {
                 break;
         }
     }
-<<<<<<< HEAD
-
-     void OnCollisionEnter(Collision other)
-      {
-         if (other.gameObject.tag == "Pickup")
-         {
-            Destroy(other.gameObject);
-         }
-    }
-=======
->>>>>>> 403b012e13619e506c6f9907971affc6937198aa
 }
