0bd4ffa1bae0646e6b492484f2236a49a64fb1cf
Therratos <1001983@student.sae.edu.au>
2016-05-03 17:43:11 +1000

    added a bunch of bird things, got a bunch more to do.

---
+++ Sticky Situation/Assets/BirdController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdController : MonoBehaviour {
+
+	public GameObject[] feedingPoints;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Sticky Situation/Assets/BirdShadowController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdShadowController : MonoBehaviour {
+	public GameObject BirdParent;
+	public Transform theBird;
+	public Transform myPos; 
+
+	// Use this for initialization
+	void Start () {
+		myPos = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3 (theBird.position.x, myPos.position.y, theBird.position.z);
+//		this.transform.rotation = Quaternion.Euler (theBird.rotation.x*90, theBird.rotation.y*90, theBird.rotation.z*90);
+		this.transform.rotation = theBird.transform.rotation;
+}
+
+	void OnTriggerEnter (Collider other)
+	{
+		
+		if (other.gameObject.name == "Player") {
+			Debug.Log ("I found an " + other.gameObject.name);
+		}
+	}
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject.name == "Player") {
+			Debug.Log ("I lost an " + other.gameObject.name);
+		}
+	}
+}
